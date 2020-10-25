89277e5684aeeed13ba727292bf54374e7dfa0e4
mmgg140yytt <73372079+mmgg140yytt@users.noreply.github.com>
2020-10-25 13:24:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzY5ODc1MzkwMTM0ODc4MjE4.X5VYVA.fW73jSFBT2P3Lr2t310AvenIlPU", // توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "&", //برفكس البوت الأساسي
+  dev : "767126436879532114" //أيديك
+};
